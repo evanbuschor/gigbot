82eebd3224288f66ec9a5ff21bf1d2b983c86151
Evan <evan.buschor@gmail.com>
2020-05-08 10:26:27 -0400

    update avatar command to display animated images

--- bot.js
+++ bot.js
@@ -32,7 +32,7 @@ client.on("message", (message) => {
 	}
 
 	if (message.content.startsWith(`${prefix}avatar`)) {
-		message.reply(message.author.displayAvatarURL());
+		message.reply(message.author.displayAvatarURL({ dynamic: true }));
 	}
 });
 
